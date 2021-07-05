b7442f6eb227a300f96707e2c9c000a0a065a5bc
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-07-05 19:37:14 -0300

    Basic math operations

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const resultMode = document.querySelector('#result-mode');
 const themeIcon = document.querySelector('#theme-icon');
 const optionsIcon = document.querySelector('#options-icon');
 const expressionInput = document.querySelector('#expression');
-const expressionResult = document.querySelector('#expression-result');
+const expressionResult = document.querySelector('#result');
 const deleteIcon = document.querySelector('#delete-icon');
 
 let currentMode;
+let expressionOperator;
 
 function initializeInterface() {
   const userTheme = localStorage.getItem('userTheme');
@@ -39,15 +40,87 @@ function toggleResultMode() {
   resultMode.textContent = currentMode == 'RAD' ? 'GRAU' : 'RAD';
 }
 
+function addNumbersOnDisplay(number) {
+  expressionInput.value += number;
+}
+
+function addOperatorsOnDisplay(operator) {
+  expressionOperator = operator;
+
+  if (operator == '*') {
+    operator = 'x';
+  } else if (operator == '/') {
+    operator = '÷';
+  }
+
+  expressionInput.value += operator;
+}
+
+function calculateResult() {
+  if ((expressionInput.value).indexOf(expressionOperator) != -1) {
+    const completeExpression = (expressionInput.value).replace('x', '*').replace('÷', '/')
+    let numbers = completeExpression.split(expressionOperator)
+
+    if (numbers[0] != '' && numbers[1] != '') {
+      numbers = [Number(numbers[0]), Number(numbers[1])];
+      let result;
+
+      switch (expressionOperator) {
+        case '+': {
+          result = numbers[0] + numbers[1];
+          break
+        }
+        case '-': {
+          result = numbers[0] - numbers[1];
+          break
+        }
+        case '*': {
+          result = numbers[0] * numbers[1];
+          break
+        }
+        case '/': {
+          result = numbers[0] / numbers[1];
+          break
+        }
+      }
+
+      display(result);
+    }
+  }
+}
+
+function display(result) {
+  expressionInput.style.height = '48px';
+  expressionInput.style.marginBottom = '20px';
+  expressionResult.style.display = 'none';
+
+  if (result) {
+    expressionInput.style.height = '32px';
+    expressionInput.style.marginBottom = '0';
+    expressionResult.style.marginBottom = '20px';
+    expressionResult.textContent = result;
+    expressionResult.style.display = 'block';
+  }
+}
+
 function clearExpressions() {
   expressionInput.value = '';
   expressionResult.textContent = '';
+
+  display();
 }
 
 function deleteLastSymbol() {
-  let expression = expressionInput.value;
-  const lastLetter = expression[expression.length - 1];
+  if (expressionInput.value) {
+    let expression = expressionInput.value;
+    let expressionArray = [];
 
-  expression = expression.replace(lastLetter, '');
-  expressionInput.value = expression;
+    for (let letter in expression) {
+      expressionArray.push(expression[letter]);
+    }
+
+    expressionArray.pop();
+    expression = expressionArray.join('');
+    expressionInput.value = expression;
+  }
 }
